97c06c62a3012c89abcfb0f9da89bba73246d3af
azulstengel-ew <azul.stengel@sciencespo.fr>
2026-06-05 17:33:17 +0200

    Update index.ts

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,8 +1,6 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
-const connectionString = process.env.DATABASE_URL!;
-
-const client = postgres(connectionString, { max: 10 });
-export const db = drizzle(client, { schema });
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
